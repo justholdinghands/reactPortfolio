ba0c317d3991a8da518cc0fd12d608705c58d482
Ema Šimášková <emsimaskova@gmail.com>
2021-08-02 11:51:09 +0200

    Added theme

---
+++ src/theme.ts
@@ -0,0 +1,25 @@
+export const theme = {
+  /*counter*/
+  cnt_primary: "#ffdab7",
+  cnt_plus: "#7ccbff",
+  cnt_minus: "#ff8b5d",
+  cnt_font_secondary: "normal 1.2em/150% 'Poppins', sans-serif",
+  cnt_font_primary: "normal 1.5em/150% 'Vollkorn', serif",
+
+  /*ht*/
+  ht_bkg: "#000000",
+  ht_primary: "#00ff00",
+  ht_hover: "#009100",
+  ht_bkg_granted: "#585858",
+  ht_denied: "#fd3131",
+  ht_bkg_denied: "#5a0000",
+  ht_font: "mono",
+
+  /*todo*/
+  todo_accent: "#ead7d7",
+  todo_base: "#f5f5f5",
+  todo_faded: "#e6e6e6",
+  todo_primary: "#7b7777",
+  todo_pale: "#ffffff",
+  todo_h1_font: "'Helvetica Neue', Helvetica, Arial, sans-serif",
+};
