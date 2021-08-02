e4ec93e17145ffc4ead06344887610ffe5a87e4d
Ema Šimášková <emsimaskova@gmail.com>
2021-08-02 11:45:35 +0200

    Added theme

---
+++ src/theme.ts
@@ -0,0 +1,25 @@
+export const theme = {
+  /*counter*/
+  cnt_primary: "#ffdab7", //txt-clr
+  cnt_plus: "#7ccbff", //plus-btn-clr
+  cnt_minus: "#ff8b5d", //minus-btn-clr
+  cnt_font_secondary: "normal 1.2em/150% 'Poppins', sans-serif",
+  cnt_font_primary: "normal 1.5em/150% 'Vollkorn', serif",
+
+  /*ht*/
+  ht_bkg: "#000000", //ht_bkg
+  ht_primary: "#00ff00", // ht_primary
+  ht_hover: "#009100", //ht_hover
+  ht_bkg_granted: "#585858", //ht_bkg_granted
+  ht_denied: "#fd3131", //ht_denied
+  ht_bkg_denied: "#5a0000", //ht_bkg_denied
+  ht_font: "mono", //ht_font
+
+  /*todo*/
+  todo_accent: "#ead7d7", //todo_accent
+  todo_base: "#f5f5f5", //todo_base
+  todo_faded: "#e6e6e6", //todo_faded
+  todo_primary: "#7b7777", //todo_primary
+  todo_pale: "#ffffff", //todo_pale
+  todo_h1_font: "'Helvetica Neue', Helvetica, Arial, sans-serif",
+};
