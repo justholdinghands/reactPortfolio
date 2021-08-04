7f6e4366e4f4ddd9955aa8f0963a6657ddbb6ed9
Ema Šimášková <emsimaskova@gmail.com>
2021-08-04 12:54:35 +0200

    deleted unneccessary comments

--- src/theme.ts
+++ src/theme.ts
@@ -1,28 +1,28 @@
 export const theme = {
   counter: {
-    primary: "#ffdab7", //txt-clr
-    plus: "#7ccbff", //plus-btn-clr
-    minus: "#ff8b5d", //minus-btn-clr
+    primary: "#ffdab7",
+    plus: "#7ccbff",
+    minus: "#ff8b5d",
     font_secondary: "normal 1.2em/150% 'Poppins', sans-serif",
     font_primary: "normal 1.5em/150% 'Vollkorn', serif",
     background: "#79bac2",
   },
   hackertyper: {
-    background: "#000000", //ht_background
-    primary: "#00ff00", // ht_primary
-    hover: "#009100", //ht_hover
-    background_granted: "#585858", //ht_background_granted
-    denied: "#fd3131", //ht_denied
-    background_denied: "#5a0000", //ht_background_denied
-    font: "mono", //ht_font
+    background: "#000000",
+    primary: "#00ff00",
+    hover: "#009100",
+    background_granted: "#585858",
+    denied: "#fd3131",
+    background_denied: "#5a0000",
+    font: "mono",
   },
   todo: {
-    accent: "#ead7d7", //todo_accent
-    base: "#f5f5f5", //todo_base
-    faded: "#e6e6e6", //todo_faded
-    primary: "#7b7777", //todo_primary
-    pale: "#ffffff", //todo_pale
+    accent: "#ead7d7",
+    base: "#f5f5f5",
+    faded: "#e6e6e6",
+    primary: "#7b7777",
+    pale: "#ffffff",
     h1_font: "'Helvetica Neue', Helvetica, Arial, sans-serif",
-    background: "#f5f5f5", //todo_pale
+    background: "#f5f5f5",
   },
 };
