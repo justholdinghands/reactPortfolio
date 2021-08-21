3121f85724d76940031f1449eca31d57882b8474
Ema Šimášková <emsimaskova@gmail.com>
2021-08-21 22:55:23 +0200

    Edited flipping

--- src/components/pexeso/Pexeso.tsx
+++ src/components/pexeso/Pexeso.tsx
@@ -1,4 +1,6 @@
-import { Component, useState } from "react";
+import { Component, useEffect, useState } from "react";
+import { faLessThanEqual } from "@fortawesome/free-solid-svg-icons";
+import { pipeline } from "stream";
 import { theme } from "../../theme";
 import Piece from "./Piece";
 import styled from "styled-components";
@@ -15,51 +17,117 @@ type Props = {
 type Card = {
   name: string;
   image: string;
+  flippedNow: boolean;
 };
 
-const cards = [
+const cards: Card[] = [
   {
     name: "a",
     image: "./imgs/worm1.jpg",
+    flippedNow: false,
   },
   {
     name: "b",
     image: "./imgs/worm2.jpg",
+    flippedNow: false,
   },
   {
     name: "c",
     image: "./imgs/worm3.jpg",
+    flippedNow: false,
   },
   {
     name: "d",
     image: "./imgs/worm4.jpg",
+    flippedNow: false,
   },
   {
     name: "e",
     image: "./imgs/worm5.jpg",
+    flippedNow: false,
   },
   {
     name: "f",
     image: "./imgs/worm6.jpg",
+    flippedNow: false,
   },
   {
     name: "g",
     image: "./imgs/worm7.jpg",
+    flippedNow: false,
   },
   {
     name: "h",
     image: "./imgs/worm8.jpg",
+    flippedNow: false,
   },
 ];
 
-const Pexeso = (props: Props) => {
-  function randomSort(array: Card[]) {
-    return array.sort(() => 0.5 - Math.random());
-  }
+const randomSort = (array: Card[]) => {
+  return array.sort(() => 0.5 - Math.random());
+};
 
+const Pexeso = (props: Props) => {
   const [matrix, setMatrix] = useState<Card[]>(
     randomSort([...cards, ...cards])
   );
+
+  const [guessedArr, setGuessedArr] = useState<Card[]>([]);
+
+  // const [flippedNowArr, setflippedNowArr] = useState<Card[]>([]);
+
+  useEffect(() => {
+    const flipped = matrix.filter((piece) => piece.flippedNow);
+    if (flipped.length === 2) {
+      setTimeout(checkMatch, 2000);
+    }
+  }, [matrix]);
+
+  const checkMatch = () => {
+    const flipped = matrix.filter((piece) => piece.flippedNow);
+
+    if (flipped[0].name === flipped[1].name) {
+      setGuessedArr((p) => [...p, ...flipped]);
+      setMatrix((p) => p.map((piece) => ({ ...piece, flippedNow: false })));
+      // setflippedNowArr([]);
+    } else {
+      setTimeout(
+        () =>
+          setMatrix((p) => p.map((piece) => ({ ...piece, flippedNow: false }))),
+        500
+      );
+    }
+  };
+
+  const flip = (index: number) => {
+    const flipped = matrix.filter((piece) => piece.flippedNow);
+    if (flipped.length === 1) {
+      // setflippedNowArr((p) => [...p, matrix[index]]);
+      setMatrix((p) =>
+        p.map((piece, i) =>
+          i === index ? { ...piece, flippedNow: true } : { ...piece }
+        )
+      );
+    } else {
+      // setflippedNowArr([matrix[index]]);
+      setMatrix((p) =>
+        p.map((piece, i) =>
+          i === index
+            ? { ...piece, flippedNow: true }
+            : { ...piece, flippedNow: false }
+        )
+      );
+    }
+  };
+
+  const checkGuessed = (index: number) => {
+    const card = matrix[index];
+    return guessedArr.some((piece) => card.name === piece.name);
+  };
+
+  console.log(matrix);
+  console.log(guessedArr);
+
   return (
     <DivWrapper>
       {matrix.map((e, index) => {
@@ -69,24 +137,12 @@ const Pexeso = (props: Props) => {
             index={index}
             name={e.name}
             image={e.image}
+            isFlippedNow={matrix[index].flippedNow}
+            isGuessed={checkGuessed(index)}
+            onClick={() => flip(index)}
           ></Piece>
         );
       })}
-      {/* <table>
-        <tbody>
-          {[...Array(4)].map((_, row) => {
-            return (
-              <tr key={row}>
-                {[...Array(4)].map((_, column) => (
-                  <td key={column}>
-                    <Piece onClick={() => {}}> </Piece>
-                  </td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table> */}
     </DivWrapper>
   );
 };
--- src/components/pexeso/Piece.tsx
+++ src/components/pexeso/Piece.tsx
@@ -2,9 +2,14 @@ import { Component, useState } from "react";
 import { theme } from "../../theme";
 import styled from "styled-components";
 
-const ButtonPiece = styled.button<{ flipped: boolean; image: string }>`
+const ButtonPiece = styled.button<{
+  flipped: boolean;
+  image: string;
+  guessed: boolean;
+}>`
   height: 20vh;
   width: 23vw;
+  background: ${(props) => (props.guessed ? "red" : "")};
   /* background: ${(props) =>
     props.flipped ? props.image : theme.pexeso.notFlipped}; */
 `;
@@ -13,18 +18,24 @@ type Props = {
   name: string;
   image: string;
   index: number;
+  isFlippedNow: boolean;
+  isGuessed: boolean;
+  onClick: () => void;
 };
 
 const Piece = (props: Props) => {
-  const [isFlipped, setFlip] = useState(false);
+  // const [isFlipped, setFlip] = useState(false);
   return (
     <ButtonPiece
-      flipped={isFlipped}
-      onClick={() => setFlip(!isFlipped)}
+      guessed={props.isGuessed}
+      flipped={props.isFlippedNow}
+      onClick={props.onClick}
       image={props.image}
     >
       {props.name}
-      {isFlipped && <img width="100%" height="100%" src={props.image} />}
+      {props.isFlippedNow && (
+        <img width="100%" height="100%" src={props.image} />
+      )}
     </ButtonPiece>
   );
 };
